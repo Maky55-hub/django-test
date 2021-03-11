bc2477d7f9c5ef0bfe4e5bb384c112bbdf7b8f4c
blackflash <blackflash@localhost.localdomain>
2021-03-11 00:57:09 -0800

    [DJ-02]added-postgresql-to-project

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'Pa$$w0rd',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
