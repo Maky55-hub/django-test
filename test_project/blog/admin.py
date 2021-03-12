ebcb9da0ed6bad02ac431b9f2a14459403cd4234
blackflash <blackflash@localhost.localdomain>
2021-03-12 03:04:23 -0800

    [DJ-02]added-post-model

--- test_project/blog/admin.py
+++ test_project/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+# Register your models here, only if you want them on the admin page
+
+admin.site.register(Post)
