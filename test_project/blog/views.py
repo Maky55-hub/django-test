e741d8a94c7cfb69039830b520a4c92a1ae22374
blackflash <blackflash@localhost.localdomain>
2021-03-12 07:40:28 -0800

    [DJ-02]Added Date Formatter
    The Date formatter is added, inorder to show the Date for the blogs in the
    right format

--- test_project/blog/views.py
+++ test_project/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author' : 'Max Mustermann',
-		'date_posted' : '04.06.2020',
-		'title' : 'First Post',
-		'content' : 'This is my first post'
-	},
-	{
-		'author' : 'Andy Labello',
-		'date_posted' : '05.06.2020',
-		'title' : 'First Post',
-		'content' : 'This is my second post'
-	}
-]
+from .models import Post
 
 def Home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
