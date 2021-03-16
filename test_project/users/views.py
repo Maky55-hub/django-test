6f6bcc88e153c91fa390cadceaa417d7fff13e65
blackflash <blackflash@localhost.localdomain>
2021-03-16 04:59:06 -0700

    [DJ-07]added profile updating form

--- test_project/users/views.py
+++ test_project/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import EmailMessage
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 
 def user_registration(request):
@@ -26,7 +26,7 @@ def user_registration(request):
 
             form.save()
             # show a success message, after a user account is created
-            messages.success(request, f'Account for {username} is created')
+            messages.success(request, f'Account for {username} is created!')
             return redirect('Blog-Home')
     else:
         form = UserRegistrationForm()
@@ -35,4 +35,25 @@ def user_registration(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm( request.POST,
+                                    request.FILES, 
+                                    instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('Profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/profile.html', context)
