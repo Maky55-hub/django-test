276c174451afdc040e51c846f050ce05a2ab4bc5
blackflash <blackflash@localhost.localdomain>
2021-03-16 03:31:29 -0700

    [DJ-06] Signals to create Profile
    added signals to created and update profiles, when a user is created or updated repectively

--- test_project/users/apps.py
+++ test_project/users/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    # when using signals, import needs to be done
+    def ready(self):
+        import users.signals
