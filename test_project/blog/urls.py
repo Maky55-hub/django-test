aed4b384e35df4b6ccc6b1832c45068c5545fc93
blackflash <blackflash@localhost.localdomain>
2021-03-17 04:33:24 -0700

    [DJ-08]UserPostListView
    Added UserPostListView inorder to display the posts of a specific user

--- test_project/blog/urls.py
+++ test_project/blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from .views import (
     PostListView,
+    UserPostListView,
     PostDetailView,
     PostCreateView,
     PostUpdateView,
@@ -13,5 +14,6 @@ urlpatterns = [
     path('post/new', PostCreateView.as_view(), name='Post-Create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='Post-Update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='Post-Delete'),
+    path('post/user/<str:username>', UserPostListView.as_view(), name='User-Posts'),
     path('', PostListView.as_view(), name='Blog-Home'),
 ]
